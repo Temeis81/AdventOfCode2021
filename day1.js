40f0632f73aa8f81f2abfafade0739e5376fb1f2
Teun Melisse <39822631+Temeis81@users.noreply.github.com>
2021-12-01 16:05:05 +0100

    Day 1 coding

---
+++ day1.js
@@ -0,0 +1,28 @@
+function compareDepths(){
+  document.getElementById('inputfile')
+            .addEventListener('change', function() {
+            var fr=new FileReader();
+            var greaterThanIterations = 0;
+            var greaterThanTripIs = 0;
+            fr.onload=function(){
+              const depthValues = fr.result.split('\n');
+              depthValues.forEach(function(currentValue, index){
+                  var prevValue = depthValues[index - 1];
+                  if(currentValue > prevValue){
+                    greaterThanIterations ++;
+                  }
+                  if(index >= 3){
+                    var currentSetValue = parseInt(currentValue) + parseInt(depthValues[index-1]) + parseInt(depthValues[index-2]);
+                    var prevSetValue = parseInt(depthValues[index - 1]) + parseInt(depthValues[index - 2]) + parseInt(depthValues[index - 3]);
+                    if (Number.isFinite(currentSetValue) && currentSetValue > prevSetValue){
+                      console.log(currentSetValue, prevSetValue);
+                      greaterThanTripIs ++;
+                    }
+                  }
+              })
+              document.getElementById('output').textContent = greaterThanIterations;
+              document.getElementById('tripoutput').textContent = greaterThanTripIs;
+            }
+            fr.readAsText(this.files[0]);
+        })
+}
